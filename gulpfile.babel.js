f7aa26c8689dc40de560818ad1a59443a5aefad3
Geoffrey Signorato <signoratog@proximity.bbdo.fr>
2018-10-09 16:59:42 +0400

    remove babel from build

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -60,9 +60,6 @@ export function clearJS() {
 export const build = () =>
     gulp
         .src(PATHS.SRC)
-        .pipe(babel({
-            presets: ['env']
-        }))
         .pipe(uglify())
         .pipe(header(banner, { pkg: pkg, now: now }))
         .pipe(rename({ suffix: '.min' }))
