27d1507fb1e94ee0dcb86ec86902806e4ad49285
gesielrosa <Timao159]>
2021-08-03 08:50:49 -0300

    update typescript typings

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,10 @@
 declare module 'simple-parallax-js' {
     interface IParallaxSettings {
-        orientation?: 'up' | 'down' | 'left' | 'right';
+        orientation?: 'up' | 'down' | 'left' | 'right' | 'up left' | 'up right' | 'down left' | 'down right';
         scale?: number;
         overflow?: boolean;
         delay?: number;
         transition?: string;
-        breakpoint?: number;
         customContainer?: string | HTMLElement;
         customWrapper?: string;
         maxTransition?: number;
